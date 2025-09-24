27b95a0f2c231ac22b7170805ee5f94a422570f3
VijayTomar-01 <vt8260712@gmail.com>
2025-09-24 03:19:16 -0700

    backend url

--- server/server.js
+++ server/server.js
@@ -23,7 +23,11 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: [
+    'https://marvelous-baklava-afdfaf.netlify.app',
+    'http://localhost:5173'
+  ],
+  credentials: true
 }))
 
 app.use((req,res,next)=>{
